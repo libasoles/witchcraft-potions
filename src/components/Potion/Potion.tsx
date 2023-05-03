956dc3a159d50c3d0d3abc3e62d33e192dac0f2a
Guillermo Perez <gperez78@gmail.com>
2023-05-03 18:29:41 -0300

    Tidy up css and image sizes

--- src/components/Potion/Potion.tsx
+++ src/components/Potion/Potion.tsx
@@ -23,7 +23,7 @@ function Potion({ name, onQuantitySelection }: Props) {
     return (
         <div className='flex flex-col w-32 p-4 box-content items-center gap-2'>
             <div className='font-semibold'>{potion.name}</div>
-            <Image src={`/images/${potion.image}`} alt={potion.name} width={40} height={80} />
+            <Image src={`/images/${potion.image}`} alt={potion.name} width={40} height={82} />
             <input type="number"
                 name="quantity"
                 role="spinbutton"
